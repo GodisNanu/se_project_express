51b83c137e2c6b9a2cbfee3d2fc099a6879060a6
Godis Nanu <godisnanu@gmail.com>
2024-11-29 14:58:53 -0500

    setting up eslint

---
+++ .eslintrc.js
@@ -0,0 +1,25 @@
+module.exports = {
+  env: {
+    es2021: true,
+    node: true,
+  },
+  extends: ["airbnb-base", "eslint:recommended", "prettier"],
+  overrides: [
+    {
+      env: {
+        node: true,
+      },
+      files: [".eslintrc.{js,cjs}"],
+      parserOptions: {
+        sourceType: "script",
+      },
+    },
+  ],
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  rules: {
+    "no-underscore-dangle": "off",
+  },
+};
