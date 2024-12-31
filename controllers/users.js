848a323062053fbb3974f8d9384b10f7588fcd2b
Godis Nanu <godisnanu@gmail.com>
2024-12-30 21:15:51 -0500

    completed postman testing

--- controllers/users.js
+++ controllers/users.js
@@ -18,12 +18,10 @@ const getUser = (req, res) => {
     .then((user) => res.status(200).send(user))
     .catch((err) => {
       console.error(err);
-      if (err.name === "ValidationError") {
+      if (err.name === "CastError") {
         return res.status(400).send({ message: err.message });
       } else if (err.name === "DocumentNotFoundError") {
         return res.status(404).send({ message: err.message });
-      } else if (err.name === "CastError") {
-        return res.status(401).send({ message: err.message });
       }
       return res.status(500).send({ message: err.message });
     });
