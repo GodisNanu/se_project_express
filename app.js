9b07bcfbdd6cd26e959e363065782fd7197d8609
Godis Nanu <godisnanu@gmail.com>
2025-02-02 23:44:21 -0500

    installed cors

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const mainRouter = require("./routes/index");
+const cors = require("cors");
 
 const { PORT = 3001 } = process.env;
 const app = express();
@@ -8,6 +9,7 @@ mongoose.connect("mongodb://127.0.0.1:27017/wtwr_db");
 
 app.use(express.json());
 app.use("/", mainRouter);
+app.use(cors());
 
 app.listen(PORT, () => {
   console.log(`App listening at port ${PORT}`);
