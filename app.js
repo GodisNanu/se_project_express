90a89bee8e35a919bfed3a7c8922bb5cd7686d3b
Godis Nanu <godisnanu@gmail.com>
2025-09-01 16:29:06 -0400

    cleaning up code

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const { errors } = require("celebrate");
 const mainRouter = require("./routes/index");
 const errorHandler = require("./middlewares/error-handler");
 const { requestLogger, errorLogger } = require("./middlewares/logger");
+
 const { PORT = 3001 } = process.env;
 const app = express();
 mongoose.connect("mongodb://127.0.0.1:27017/wtwr_db");
