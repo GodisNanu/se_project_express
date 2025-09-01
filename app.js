30deecb07cbb570350145b5908ca4847e8587108
Godis Nanu <godisnanu@gmail.com>
2025-08-31 22:14:43 -0400

    fixing errors

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
+const { errors } = require("celebrate");
 const mainRouter = require("./routes/index");
 const errorHandler = require("./middlewares/error-handler");
-const { errors } = require("celebrate");
 
 const { PORT = 3001 } = process.env;
 const app = express();
--- middlewares/validation.js
+++ middlewares/validation.js
@@ -1,5 +1,7 @@
 const { Joi, celebrate } = require("celebrate");
+
 const validator = require("validator");
+
 const validateURL = (value, helpers) => {
   if (validator.isURL(value)) {
     return value;
