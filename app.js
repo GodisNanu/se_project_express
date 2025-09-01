5e0e3419947a4b6fc81d92ec9116bc37574e8353
Godis Nanu <godisnanu@gmail.com>
2025-08-31 22:05:06 -0400

    validating inbound server data

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose");
 const cors = require("cors");
 const mainRouter = require("./routes/index");
 const errorHandler = require("./middlewares/error-handler");
+const { errors } = require("celebrate");
 
 const { PORT = 3001 } = process.env;
 const app = express();
@@ -11,6 +12,7 @@ mongoose.connect("mongodb://127.0.0.1:27017/wtwr_db");
 app.use(express.json());
 app.use(cors());
 app.use("/", mainRouter);
+app.use(errors());
 app.use(errorHandler);
 
 app.listen(PORT, () => {
---
+++ middlewares/vslidation.js
@@ -0,0 +1,72 @@
+const { Joi, celebrate } = require("celebrate");
+const validator = require("validator");
+const validateURL = (value, helpers) => {
+  if (validator.isURL(value)) {
+    return value;
+  }
+  return helpers.error("string.uri");
+};
+const hexIdRegex = /^[0-9a-fA-F]{24}$/;
+
+module.exports.validateCardBody = celebrate({
+  body: Joi.object().keys({
+    name: Joi.string().required().min(2).max(30).messages({
+      "string.min": 'The minimum length of the "name" field is 2',
+      "string.max": 'The maximum length of the "name" field is 30',
+      "string.empty": 'The "name" field must be filled in',
+    }),
+
+    imageUrl: Joi.string().required().custom(validateURL).messages({
+      "string.empty": 'The "imageUrl" field must be filled in',
+      "string.uri": 'the "imageUrl" field must be a valid url',
+    }),
+  }),
+});
+
+module.exports.validateUserInfo = celebrate({
+  body: Joi.object().keys({
+    name: Joi.string().required().min(2).max(30).messages({
+      "string.empty": 'The "name" field must be filled in',
+      "string.min": 'The minimum length of the "name" field is 2',
+      "string.max": 'The maximum length of the "name" field is 30',
+    }),
+
+    avatar: Joi.string().required().custom(validateURL).messages({
+      "string.empty": 'The "avatar" field must be filled in',
+      "string.uri": 'the "avatar" field must be a valid url',
+    }),
+
+    email: Joi.string().required().email().messages({
+      "string.empty": 'The "email" field must be filled in',
+      "string.email": 'The "email" field must be a valid email',
+    }),
+
+    password: Joi.string().required().min(8).messages({
+      "string.empty": 'The "password" field must be filled in',
+      "string.min": 'The minimum length of the "password" field is 8',
+    }),
+  }),
+});
+
+module.exports.validateAuthentication = celebrate({
+  body: Joi.object().keys({
+    email: Joi.string().required().email().messages({
+      "string.empty": 'The "email" field must be filled in',
+      "string.email": 'The "email" field must be a valid email',
+    }),
+
+    password: Joi.string().required().min(8).messages({
+      "string.empty": 'The "password" field must be filled in',
+      "string.min": 'The minimum length of the "password" field is 8',
+    }),
+  }),
+});
+
+module.exports.validateId = celebrate({
+  params: Joi.object().keys({
+    id: Joi.string()
+      .required()
+      .pattern(hexIdRegex)
+      .message("Id must be a valid 24-character hexadecimal string"),
+  }),
+});
